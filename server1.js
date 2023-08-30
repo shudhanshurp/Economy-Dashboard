67d559e44bfda7c8debb8b2fe833f246c6fea7e0
Sudhanshu14014 <shudhanshurp@gmail.com>
2023-08-30 15:59:42 -0500

    minor fixes

--- server1.js
+++ server1.js
@@ -32,46 +32,46 @@ app.use(express.static(viewsPath));
 // app.set('views',pugPath);
 
 app.get("/", (req, res) => {
-  // res.sendFile(__dirname + '/HomePage.html');
-//   res.render("index");
+    // res.sendFile(__dirname + '/HomePage.html');
+    //   res.render("index");
 });
 
 app.get("/gdp", (req, res) => {
-//   res.render("gdp");
+    //   res.render("gdp");
     // res.end("this is the gdp");
     res.sendFile(viewsPath + "/gdp.html");
 });
 
 app.get("/import", (req, res) => {
-//   res.render("import");
+    //   res.render("import");
     // res.end("this is the import");
     res.sendFile(viewsPath + "/import.html");
 });
 
 app.get("/export", (req, res) => {
-  //   res.render("export");
-  // res.end("this is the export");
-  res.sendFile(viewsPath + "/export.html");
+    //   res.render("export");
+    // res.end("this is the export");
+    res.sendFile(viewsPath + "/export.html");
 });
 
 app.get("/business", (req, res) => {
-//   res.render("business");
+    //   res.render("business");
     // res.end("this is the business");
     res.sendFile(viewsPath + "/business.html");
 });
 
 app.get("/goverment", (req, res) => {
-//   res.render("government");
+    //   res.render("government");
     // res.end("this is the goverment");
     res.sendFile(viewsPath + "/goverment.html");
 });
 
 app.get("/other", (req, res) => {
-//   res.render("other");
+    //   res.render("other");
     // res.end("this is the other incadicater ");
     res.sendFile(viewsPath + "/other.html");
 });
 
 app.listen(port, () => {
-  console.log("Server run   ning at port no " + port);
+    console.log("Server running at port no " + port);
 });
